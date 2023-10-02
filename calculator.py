efc32a7b344f6082d1b0bc14d051dade2d819546
tarugaru <tallulahcook@gmail.com>
2023-10-02 11:41:34 -0700

    Create calculator.py
    Python file for calculator implementation

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Tallulah Cook
+# CS 480
+# Calculator Application:
+    # objective: implement a simple calculator that takes in user input (Strings)
+# 10/2/23 - 
+# calculator.py
+
+print("Hello World")
