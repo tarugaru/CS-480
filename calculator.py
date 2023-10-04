635b58200f0fc9c41c5ee2b727c9eb2a74e3b347
tarugaru <tallulahcook@gmail.com>
2023-10-03 20:27:28 -0700

    new method
    going with global stack/queue/expression... more functions to handle input types

--- calculator.py
+++ calculator.py
@@ -4,35 +4,50 @@
     # objective: implement a simple calculator that takes in user input (Strings)
 # 10/2/23 - 
 # calculator.py
-def evaluate(expr):
-    #shunting yard algorithm-- need a stack [operators] and queue[postfix]
-    operators = []
-    postfix = []
-    while len(expr)>0:
-        element = expr.pop(0)
-        if element.isdigit() and len(expr)>0:
-            char = expr[0]
-            while char.isdigit():
-                element = element + char
-                expr.pop(0)
-                char = expr[0]
-            postfix.append(element)
-        elif element.isdigit():
-            postfix.append(element)
+operators =[]
+postfix = []
+expression = []
+
+def parseExp():
+    while len(expression)>0:
+        element = expression.pop(0)
+        if element.isdigit() or element == ".":
+            getNumVal(element)
+        elif element == "+" or "*":
+            evalOp(element)
+        print(postfix)
+        print(operators)
+
+def getNumVal(element):
+    char = None
+    if len(expression)>0:
+        char = expression[0]
+    while char != None:
+        if char.isdigit() or char=="." or char=="^":
+            element = element + char
+            expression.pop(0)
+            if len(expression)> 0:
+                char = expression[0]
+            else:
+                char = None
         else:
-            #if it is not a digit, but not a ')' first look at the stack to see if a higher
-            #precedence operation takes place. if it is a ')' add in all operators until '('
-            #maybe a good place for bracket/parenthesis error checking?
-            operators.append(element)
-    print(postfix)
-    print(operators)
-    #after all checking is added, should end up with an empty operators stack, 
-    #and a full queue
+            char = None
+    postfix.append(element)
     
-expression = input("Evaluate: ")
+def evalOp(element):
+    if len(operators) >0:
+        prevOp = operators[len(operators)-1]
+        if (prevOp == "*" or prevOp == "/") and (element == "+" or element == "-"):
+            postfix.append(prevOp)
+            operators.pop()
+            operators.append(element)
+    else:
+        operators.append(element)
+        
+expr = input("Evaluate: ")
 #get rid of spaces in user input
-expression = expression.replace(" ","")
-expr = []
-expr = list(expression)
-print(expr)
-evaluate(expr)
+expr = expr.replace(" ","")
+expression = list(expr)
+print(expression)
+parseExp()
+
