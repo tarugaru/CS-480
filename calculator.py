2668394997850741ca971247eeb36075b0b0ab2b
tarugaru <tallulahcook@gmail.com>
2023-10-04 17:32:41 -0700

    minus/negative characters

--- calculator.py
+++ calculator.py
@@ -9,21 +9,26 @@ postfix = []
 expression = []
 
 def parseExp():
+    if expression[0]=="-":
+        getNumVal(expression.pop(0))
     while len(expression)>0:
         element = expression.pop(0)
         if element.isdigit() or element == ".":
             getNumVal(element)
-        elif element == "+" or "*":
+        elif element == "+" or "*" or "-":
             evalOp(element)
         print(postfix)
         print(operators)
 
 def getNumVal(element):
     char = None
+    expFlag = bool(False)
     if len(expression)>0:
         char = expression[0]
     while char != None:
         if char.isdigit() or char=="." or char=="^":
+            if char == "^":
+                expFlag = bool(True)
             element = element + char
             expression.pop(0)
             if len(expression)> 0:
@@ -32,6 +37,12 @@ def getNumVal(element):
                 char = None
         else:
             char = None
+    if expFlag:
+        num =element.split("^")
+        base = (float)(num[0])
+        exp = (float)(num[1])
+        element = base**exp
+    element = (float)(element)
     postfix.append(element)
     
 def evalOp(element):
@@ -43,7 +54,9 @@ def evalOp(element):
             operators.append(element)
     else:
         operators.append(element)
-        
+    if expression[0] == "-":
+        getNumVal(expression.pop(0))
+    
 expr = input("Evaluate: ")
 #get rid of spaces in user input
 expr = expr.replace(" ","")
