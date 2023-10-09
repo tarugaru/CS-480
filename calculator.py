b3134d82cb87cbf870783358e2d10b9b43a756aa
tarugaru <tallulahcook@gmail.com>
2023-10-09 11:46:53 -0700

    update with sin,cos,log,ln infrastructure

--- calculator.py
+++ calculator.py
@@ -8,18 +8,76 @@ operators =[]
 postfix = []
 expression = []
 
+
 def parseExp():
+    validExp = True
     if expression[0]=="-":
         getNumVal(expression.pop(0))
-    while len(expression)>0:
+    while len(expression)>0 and validExp:
         element = expression.pop(0)
         if element.isdigit() or element == ".":
             getNumVal(element)
+        elif element == "c":
+            validExp = evalCos(element)
+        elif element == "s":
+            evalSin(element)
+        elif element == "l":
+            evalLog(element)
+        elif element == "n":
+            evalLn(element)
         elif element == "+" or "*" or "-":
             evalOp(element)
         print(postfix)
         print(operators)
+        
+def evalCos(element):
+    validCos= True
+    print("eval cos")
+    if expression[0]=="(":
+        expression.pop(0)
+        #evaluate the inside of cos
+        print("confirmed")
+    else:
+        validCos = False
+        print("invalid cos")
+    return validCos
 
+def evalSin(element):
+    validSin= True
+    print("eval sin")
+    if expression[0]=="(":
+        expression.pop(0)
+        #evaluate the inside of cos
+        print("confirmed")
+    else:
+        validSin = False
+        print("invalid cos")
+    return validSin
+    
+def evalLog(element):
+    validLog= True
+    print("eval log")
+    if expression[0]=="(":
+        expression.pop(0)
+        #evaluate the inside of cos
+        print("confirmed")
+    else:
+        validLog = False
+        print("invalid log")
+    return validLog
+    
+def evalLn(element):
+    validLn= True
+    print("eval ln")
+    if expression[0]=="(":
+        expression.pop(0)
+        #evaluate the inside of cos
+        print("confirmed")
+    else:
+        validLn = False
+        print("invalid log")
+    return validLn
+    
 def getNumVal(element):
     char = None
     expFlag = bool(False)
@@ -60,6 +118,11 @@ def evalOp(element):
 expr = input("Evaluate: ")
 #get rid of spaces in user input
 expr = expr.replace(" ","")
+expr = expr.replace("sin", "s")
+expr = expr.replace("cos","c")
+expr = expr.replace("log", "l")
+expr = expr.replace("ln", "n")
+print(expr)
 expression = list(expr)
 print(expression)
 parseExp()
