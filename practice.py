b3f629f2d1647787e98ef1049ea8460dcc8119cf
tarugaru <tallulahcook@gmail.com>
2023-10-10 12:47:20 -0700

    merging files

--- practice.py
+++ practice.py
@@ -4,10 +4,37 @@ Created on Mon Oct  9 21:10:58 2023
 
 @author: tallu
 """
-def evalExp(op,cp):
-    print(str(op) + " " + str(cp))
+def evalExp(expr):
+    postfix = []
+    operators = []
+    print(expr)
+    ex_list = list(expr)
+    validExp = True
+    '''
+    if ex_list[0]=="-":
+        getNumVal(ex_list.pop(0))
+    while len(ex_list)>0 and validExp:
+        element = expression.pop(0)
+        if element.isdigit() or element == ".":
+            validExp = getNumVal(element)
+        elif element == "c":
+            validExp = evalCos(element)
+        elif element == "s":
+            evalSin(element)
+        elif element == "l":
+            evalLog(element)
+        elif element == "n":
+            evalLn(element)
+        elif element == "+" or element =="*" or element=="-":
+            print("made it")
+            evalOp(element)
+        '''
+    print(postfix)
+    print(operators)
+        
     #here is where we evaluate what is going on inside this particular parenthesis
     #lets create the shunting yard algo here, with the num queue and op stack
+    return 0
 class point(object):
     def __init__(self,open_par,close_par):
         self.OP = open_par
@@ -20,7 +47,9 @@ class point(object):
         return "( is " + str(self.OP) +"\n) is " + str(self.CP)
         
 user_input = input("Evaluate: ")
-expression = (list)("(" + user_input + ")")
+string = "(" + user_input + ")"
+print(string)
+expression = (list)(string)
 print(expression)
 open_pars = []
 close_pars = []
@@ -43,4 +72,8 @@ while iterator < len(par_pairs):
     pair = par_pairs.pop(0)
     op = pair.getOP()
     cp = pair.getCP()
-    evalExp(op,cp)
+    expr = string[op+1:cp]
+    print(expr)
+    number = evalExp(expr)
+    iterator+=1
+
